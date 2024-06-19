183a371763edafd4f48e13f23224c6600d52bb11
readwanmd <readwanmd@gmail.com>
2024-06-19 13:54:52 +0600

    cores back to original position

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
 
-app.use(cors());
-app.use(express.json());
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 connectDB();
 
@@ -20,6 +21,6 @@ app.use('/api/events', require('./routes/eventRoutes'));
 app.use('/api/bookings', require('./routes/bookingRoutes'));
 app.use('/api/payments', require('./routes/paymentRoutes'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
